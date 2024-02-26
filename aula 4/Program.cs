38506405445c6f3940e1dfdc7833d42ba5ce2ca5
ALUNO <ALUNO@LABFPC36>
2024-02-26 08:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ aula 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if(a%4 == 0) 
+            {
+                Console.WriteLine("é divisivel por 4");
+            }
+            else
+            {
+                Console.WriteLine("não é divisivel por 4");
+            }
+            Console.ReadKey();  
+
+        }
+    }
+}
